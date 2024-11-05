6f3323cd1d067c1073d625469cf53987dd064138
Kevin Calle <kcalle012@gmail.com>
2024-11-05 16:45:14 -0500

    testing dns

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meet Kevin.tech
         </a>
       </header>
     </div>

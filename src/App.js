572422256ee517de76fa86613be5bdaf7aa44142
Kevin Calle <kcalle012@gmail.com>
2024-11-06 18:29:26 -0500

    fixed picture

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
-import './Components/Home/index';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/index';
 import './Components/Contact/index';
-import './Components/About';
+import './Components/About/index';
+import  Layout from './Components/Layout/index'
 function App() {
   return (
+    <>
+    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />}/>
-        <Route path="/contact" element={<Contact />}/>
-        <Route path="/about" element={<About />}/>
-        <Route />
-      </Route>
+         <Route path="/" element={<Layout />}>
+           <Route index element={<Home />}/>
+           {/* <Route path="/contact" element={<Contact />}/> */}
+           {/* <Route path="/about" element={<About />}/> */}
+           {/* <Route /> */}
+         </Route>
     </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 

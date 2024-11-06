c3838dc87d38aa2babe5bda4f84db1895144e3df
Kevin Calle <kcalle012@gmail.com>
2024-11-06 18:23:36 -0500

    started front page

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,12 +1,14 @@
 import { React } from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../Header/index';
+// import { Footer } from '../Footer/index'
 
 const Layout = () => {
     return (
         <div>
             <Header />
             <Outlet />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 } 

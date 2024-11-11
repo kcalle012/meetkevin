68eeec48ef5e5f1f94601ae2b12d50ebf50da265
Kevin Calle <kcalle012@gmail.com>
2024-11-11 10:52:04 -0500

    added headshot and added About Me section to the home page

---
+++ src/Components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.scss'
+
+const About = () => {
+    return (
+        <div className='container'>
+            <div className="img-container">
+                <img src="" alt="" />
+            </div>
+            <h1>
+            Hello! My name is Kevin Calle and I’m a computer science student at Montclair State University with a passion for web development, AI, and backend technologies. 
+            I’m currently working on projects like an AI model for detecting AI-generated videos and an interactive React-based web app. 
+            Through my research, I helped develop a text-to-video detector that achieves 92% accuracy. 
+            Curious and solution-oriented, I approach challenges with a unique perspective. 
+            Recently, I participated in a steampunk-themed hackathon and am developing a facial mood recognition app. 
+            I’m excited to keep learning and exploring opportunities in software development, especially with tech leaders.
+            </h1>
+        </div>
+    )
+}
+
+export default About;
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,14 +2,18 @@ import { React } from 'react';
 import './index.scss'
 import { useState, useEffect } from 'react';
 import HelloMessage from '../HelloMessage';
+import About from '../About';
 import { motion } from "framer-motion"
 
 function renderHello (){
     return <div><HelloMessage /></div>
 }
+function renderAbout (){
+    return <div><About /></div>
+}
 
 const Home = () => {
-    const sections = [renderHello];
+    const sections = [renderHello, renderAbout];
 
 
     return (

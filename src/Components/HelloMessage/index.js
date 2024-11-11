89b35f67168bf86464ee8b10be64e9b687ed1e3d
Kevin Calle <kcalle012@gmail.com>
2024-11-10 22:20:09 -0500

    Fixed mobile issues:#

--- src/Components/HelloMessage/index.js
+++ src/Components/HelloMessage/index.js
@@ -8,7 +8,7 @@ import { TypeAnimation } from 'react-type-animation'
 
 
 const HelloMessage = () => {
-    const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState(true);
 
     useEffect(() => { 
         const handleResize = () => { 
@@ -22,20 +22,15 @@ const HelloMessage = () => {
             window.removeEventListener('resize', handleResize); 
         }; 
     },[]);
-    // const textStyle = {
-    //     fontSize: isMobile ? '1.5em': '3em',
-    //     display:'flex',
-    //     textAlign:'center',
-    //     fontFamily:'Poppins',
-    //     fontWeight:200
-    // };
+    console.log('isMobile: ', isMobile);
 
     return (
         <motion.div className='HelloMessageContainer'>
             <motion.div className = "home"></motion.div>
             <motion.div className= "bottom">
+                {isMobile ? (
                 <TypeAnimation
-                    className='intro'
+                key={isMobile ? 'mobile' : 'desktop'}
                     sequence={[
                     // Same substring at the start will only be typed out once, initially
                         'Kevin Calle: Software Engineer',
@@ -50,7 +45,7 @@ const HelloMessage = () => {
                     wrapper="span"
                     // style={textStyle}
                     style={{
-                        fontSize: isMobile ? '1.5em': '3em',
+                        fontSize:'1.5em',
                         display:'flex',
                         textAlign:'center',
                         fontFamily:'Poppins',
@@ -58,7 +53,35 @@ const HelloMessage = () => {
                     }}
                     speed={50}
                     repeat={Infinity}
-                />
+                    />
+                    ) : (
+                    <TypeAnimation
+                    key={isMobile ? 'mobile' : 'desktop'}
+                    sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                        'Kevin Calle: Software Engineer',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'Kevin Calle: CS Student',
+                        1000,
+                        'Kevin Calle: Tech Enthusiast',
+                        1000,
+                        'Kevin Calle: ',
+                        1000
+                    ]}
+                    wrapper="span"
+                    // style={textStyle}
+                    style={{
+                        fontSize:'3em',
+                        display:'flex',
+                        textAlign:'center',
+                        fontFamily:'Poppins',
+                        fontWeight:200
+                    }}
+                    speed={50}
+                    repeat={Infinity}
+                    />
+                )
+            }
             </motion.div>
         </motion.div>
     );

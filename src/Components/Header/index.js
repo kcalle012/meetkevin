475b5a13c319cf5bcc6fb490e29cd74eb700cba1
Kevin Calle <kcalle012@gmail.com>
2024-11-10 21:12:58 -0500

    pushing hamburger

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,22 @@
-import { React } from 'react'
+import { React, useState } from 'react'
 import './index.scss'
 
 const Header = () => {
+    const [menuOpen,setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className="header">
             <a href="/" className="logo">Meet Kevin</a>
-            
-            <nav className="nav-bar">
+            <div className="hamburger" onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <nav className={`nav-bar ${menuOpen ? 'open': ''}`}>
+                <div className="close-button" onClick={toggleMenu}>
+                    &times;
+                </div>
                 <a href="/">Home</a>
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>

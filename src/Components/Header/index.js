11409bf4d9de466895175a25381563589b97ff73
Kevin Calle <kcalle012@gmail.com>
2025-03-22 20:21:59 -0400

    fixing resume display

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,13 +1,31 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "./index.scss";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Resume from "../../Assets/kevin_calle_resume.pdf";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const [openModal, setOpenModal] = useState(false);
+  const [resize, setResize] = useState(window.innerWidth < 900);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setResize(window.innerWidth < 900); // Update state based on window size
+    };
+
+    window.addEventListener("resize", handleResize); // Add resize event listener
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // Clean up listener on unmount
+    };
+  }, []);
 
   const item = {
     exit: {
@@ -47,7 +65,7 @@ const Header = () => {
               &times;
             </div>
             <div className="a-link" onClick={() => setMenuOpen(false)}>
-              <a href="/kevin_calle_resume.pdf" target="_blank" rel="noreferrer noopener">
+              <div onClick={() => setOpenModal(true)}>
                 <motion.p
                   initial={{ y: 80, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
@@ -63,7 +81,7 @@ const Header = () => {
                 >
                   Resume
                 </motion.p>
-              </a>
+              </div>
             </div>
             <div className="a-link" onClick={() => setMenuOpen(false)}>
               <a href="/">
@@ -125,6 +143,28 @@ const Header = () => {
           </motion.div>
         )}
       </AnimatePresence>
+      <Modal open={openModal} onClose={() => setOpenModal(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <img
+            src={Resume}
+            alt="Resume for Kevin Calle"
+            style={{
+              maxWidth: resize ? "100vw" : "100%",
+              maxHeight: "100%",
+            }}
+          />
+        </Box>
+      </Modal>
     </div>
   );
 };
